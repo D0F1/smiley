64e27580c288e7c8c5965e8bc0523100751b7f7a
D0F1 <77449973+D0F1@users.noreply.github.com>
2021-01-14 16:01:14 +0100

    lagt til funksjon for a og b knapper

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
 })
